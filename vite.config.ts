3d1db31c9b34fa8f151122c689338495f73be77f
Darya <vikhorevadv@gmail.com>
2023-09-14 17:49:44 +0700

    preset

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,37 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import mkcert from 'vite-plugin-mkcert'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import Vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    mkcert(),
+    Vue(),
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(__dirname, './src/assets/icons')],
+      symbolId: 'icon-[name]'
+    })
   ],
+  server: {
+    port: 3000,
+    host: true,
+    https: false,
+
+    cors: {
+      origin: '*'
+    }
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src'),
+      images: path.resolve(__dirname, './src/assets/images'),
+      icons: path.resolve(__dirname, './src/assets/icons'),
+      fonts: path.resolve(__dirname, './src/assets/fonts'),
+      styles: path.resolve(__dirname, './src/themes')
     }
+  },
+  build: {
+    outDir: path.resolve(__dirname, 'build')
   }
 })
