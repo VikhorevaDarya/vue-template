8ab5dcafc0ac03be0d6a25b424974294c6ef23da
Darya <vikhorevadv@gmail.com>
2023-09-15 14:36:55 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,33 @@
-import { defineConfig } from 'vite'
-import mkcert from 'vite-plugin-mkcert'
+import { defineConfig, loadEnv } from 'vite'
+import { resolve } from 'path'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 import Vue from '@vitejs/plugin-vue'
-import path from 'path'
 
-export default defineConfig({
-  plugins: [
-    mkcert(),
-    Vue(),
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(__dirname, './src/assets/icons')],
-      symbolId: 'icon-[name]'
-    })
-  ],
-  server: {
-    port: 3000,
-    host: true,
-    https: false,
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
-    cors: {
-      origin: '*'
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      images: path.resolve(__dirname, './src/assets/images'),
-      icons: path.resolve(__dirname, './src/assets/icons'),
-      fonts: path.resolve(__dirname, './src/assets/fonts'),
-      styles: path.resolve(__dirname, './src/themes')
-    }
-  },
-  build: {
-    outDir: path.resolve(__dirname, 'build')
-  }
-})
+  return defineConfig({
+    server: {
+      port: parseInt(process.env.VITE_DEV_SERVER_PORT),
+      host: true,
+    },
+    plugins: [
+      createSvgIconsPlugin({
+        iconDirs: [resolve(__dirname, './src/assets/icons')],
+        symbolId: 'icon-[name]',
+      }),
+      ViteImageOptimizer(),
+      Vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+        images: resolve(__dirname, './src/assets/images'),
+        icons: resolve(__dirname, './src/assets/icons'),
+        fonts: resolve(__dirname, './src/assets/fonts'),
+        styles: resolve(__dirname, './src/styles'),
+      },
+    },
+  })
+}
